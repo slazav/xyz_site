bbeb9e333910e18fb044e6b1f989309edf3ff5af
Vladislav Zavjalov <slazav@altlinux.org>
2018-01-06 22:43:22 +0200

    add missing file cgi/com_screen.pl

---
+++ cgi/com_screen.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+################################################
+
+use strict;
+use warnings;
+use Try::Tiny;
+use CGI ':standard';
+use JSON;
+use site;
+use common;
+use open ':std', ':encoding(UTF-8)';
+
+################################################
+
+try {
+  screen_comment(undef, param('id') || 0);
+
+  print header (-type=>'application/json', -charset=>'utf-8');
+  print JSON->new->encode({"ret" => 0}), "\n";
+}
+catch {
+  chomp;
+  write_log($obj_log, "$0 error: $_");
+  print header (-type=>'application/json', -charset=>'utf-8');
+  print JSON->new->canonical()->encode({"ret" => 1, "error_message" => $_}), "\n";
+}
